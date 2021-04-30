807998770c204528a77a7f46f12f81975d5ee6a7
demmyhonore <demmyhonore@gmail.com>
2021-04-30 14:43:03 +0200

    feat(project): add dropdown component

---
+++ src/components/Dropdown/Dropdown.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import styles from './Dropdown.module.scss';
+
+type DropdownProps = {
+  name: string;
+  value: string;
+  defaultLabel: string;
+  options: string[];
+  setValue: ((value: string) => void);
+};
+
+function Dropdown({
+  name,
+  value,
+  defaultLabel,
+  options,
+  setValue
+}: DropdownProps) {
+
+  const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => setValue(event.target.value)
+
+  return (
+    <div className={styles.dropdown}>
+      <select name={name} value={value} onChange={handleChange}>
+        <option value="">{defaultLabel}</option>
+        {options.map(option => <option key={option} value={option}>{option}</option>)}
+      </select>
+      <span className="focus"></span>
+    </div>
+  );
+}
+
+export default Dropdown;
