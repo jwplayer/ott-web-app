360d77413e8e93dff297661a7ec06625edf1bcbb
demmyhonore <demmyhonore@gmail.com>
2021-04-30 14:44:25 +0200

    feat(project): add playlist types

---
+++ types/playlist.d.ts
@@ -0,0 +1,35 @@
+export type Image = {
+    src: string;
+    type: string;
+    width: number;
+};
+
+export type Source = {
+    file: string;
+    type: string;
+};
+
+export type Track = {
+    file: string;
+    kind: string;
+};
+
+export type PlaylistItem = {
+    description: string;
+    duration: number;
+    feedid: string;
+    image: string;
+    images: Image[];
+    link: string;
+    genre: string;
+    mediaid: string;
+    pubdate: number;
+    rating: string;
+    sources: Source[];
+    tags: string;
+    title: string;
+    tracks: Track[];
+    variations?: Record<string, unknown>;
+};
+
+export type Playlist = PlaylistItem[]
