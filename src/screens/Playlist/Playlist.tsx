235c99c6b02e3096c134837f07bd6ce68226348e
Robin van Zanten <38441984+RCVZ@users.noreply.github.com>
2021-05-06 00:30:27 +0200

    feat(playlist): add virtualizedgrid to playlist

--- src/screens/Playlist/Playlist.tsx
+++ src/screens/Playlist/Playlist.tsx
@@ -1,6 +1,8 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
 
+import VirtualizedGrid from '../../components/VirtualizedGrid/VirtualizedGrid';
 import Layout from '../../components/Layout/Layout';
 import usePlaylist from '../../hooks/usePlaylist';
 import {
@@ -9,16 +11,33 @@ import {
 } from '../../utils/collection';
 import Card from '../../components/Card/Card';
 import Dropdown from '../../components/Filter/Filter';
-import CardGrid from '../../components/CardGrid/CardGrid';
+import useBreakpoint, { Breakpoint } from '../../hooks/useBreakpoint';
 
 import styles from './Playlist.module.scss';
 
+const cols = {
+  [Breakpoint.xs]: 2,
+  [Breakpoint.sm]: 2,
+  [Breakpoint.md]: 2,
+  [Breakpoint.lg]: 4,
+  [Breakpoint.xl]: 5,
+};
+
+const chunk = (input: any[], size: number) => {
+  return input.reduce((arr: any, item: any, idx: number) => {
+    return idx % size === 0
+      ? [...arr, [item]]
+      : [...arr.slice(0, -1), [...arr.slice(-1)[0], item]];
+  }, []);
+};
+
 function Playlist() {
+  const breakpoint: Breakpoint = useBreakpoint();
   const { id: playlistId } = useParams<Record<string, string>>();
+  const [filter, setFilter] = useState<string>('');
   const { isLoading, error, data: { title, playlist } = {} } = usePlaylist(
     playlistId,
   );
-  const [filter, setFilter] = useState<string>('');
 
   if (isLoading) return <p>Loading...</p>;
 
@@ -26,6 +45,27 @@ function Playlist() {
 
   const categories = getCategoriesFromPlaylist(playlist);
   const filteredPlaylist = filterPlaylistCategory(playlist, filter);
+  const twoDplaylist = chunk(filteredPlaylist, cols[breakpoint]);
+
+  const cellRenderer = ({ columnIndex, key, rowIndex, style }: any) => {
+    if (!twoDplaylist[rowIndex][columnIndex]) return;
+    const { mediaid: mediaId, title, duration, image, seriesId } = twoDplaylist[
+      rowIndex
+    ][columnIndex];
+    console.info(twoDplaylist);
+    return (
+      <div className={styles.wrapper} style={style} key={key}>
+        <Card
+          key={mediaId}
+          title={title}
+          duration={duration}
+          posterSource={image}
+          seriesId={seriesId}
+          onClick={() => ''}
+        />
+      </div>
+    );
+  };
 
   return (
     <Layout>
@@ -43,20 +83,10 @@ function Playlist() {
           )}
         </header>
         <main>
-          <CardGrid>
-            {filteredPlaylist.map(
-              ({ mediaid: mediaId, title, duration, image, seriesId }) => (
-                <Card
-                  key={mediaId}
-                  title={title}
-                  duration={duration}
-                  posterSource={image}
-                  seriesId={seriesId}
-                  onClick={() => ''}
-                />
-              ),
-            )}
-          </CardGrid>
+          <VirtualizedGrid
+            length={twoDplaylist.length}
+            cellRenderer={cellRenderer}
+          />
         </main>
       </div>
     </Layout>
