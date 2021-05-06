e6b03ecd72d6ad76018927c6f8ae0dc832baa26d
Christiaan Scheermeijer <christiaan@videodock.com>
2021-05-06 11:24:18 +0200

    chore(project): disable build optimizations

--- snowpack.config.js
+++ snowpack.config.js
@@ -15,33 +15,33 @@ module.exports = {
     '@snowpack/plugin-react-refresh',
     '@snowpack/plugin-dotenv',
     '@snowpack/plugin-sass',
-    ['@snowpack/plugin-webpack', {
-      extendConfig: (config) => {
-        // FIXES https://github.com/snowpackjs/snowpack/discussions/2810
-        config.module.rules.find(
-          (rule) =>
-            rule &&
-            rule.use &&
-            rule.use.find((use) => {
-              if (
-                !use ||
-                !use.loader ||
-                !use.loader.includes('babel-loader')
-              ) {
-                return null;
-              }
-
-              use.options.plugins = (use.options.plugins || []).concat([
-                '@babel/plugin-proposal-optional-chaining',
-                '@babel/plugin-proposal-nullish-coalescing-operator'
-              ]);
-
-              return use;
-            }),
-        );
-        return config;
-      },
-    }],
+    // ['@snowpack/plugin-webpack', {
+    //   extendConfig: (config) => {
+    //     // FIXES https://github.com/snowpackjs/snowpack/discussions/2810
+    //     config.module.rules.find(
+    //       (rule) =>
+    //         rule &&
+    //         rule.use &&
+    //         rule.use.find((use) => {
+    //           if (
+    //             !use ||
+    //             !use.loader ||
+    //             !use.loader.includes('babel-loader')
+    //           ) {
+    //             return null;
+    //           }
+    //
+    //           use.options.plugins = (use.options.plugins || []).concat([
+    //             '@babel/plugin-proposal-optional-chaining',
+    //             '@babel/plugin-proposal-nullish-coalescing-operator'
+    //           ]);
+    //
+    //           return use;
+    //         }),
+    //     );
+    //     return config;
+    //   },
+    // }],
     [
       '@snowpack/plugin-typescript',
       {
