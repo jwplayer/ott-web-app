c2513f5730ff4834fa44a1d55b1b518a9066bc5a
Robin van Zanten <38441984+RCVZ@users.noreply.github.com>
2021-05-06 16:05:03 +0200

    feat(project): add chunk collection util

--- src/utils/collection.ts
+++ src/utils/collection.ts
@@ -1,14 +1,26 @@
 import type { Playlist } from 'types/playlist';
 
 const getCategoriesFromPlaylist = (playlist: Playlist) =>
-    playlist.reduce((categories: string[], item) => categories.includes(item.genre) ? categories : categories.concat(item.genre), [])
-
+  playlist.reduce(
+    (categories: string[], item) =>
+      categories.includes(item.genre)
+        ? categories
+        : categories.concat(item.genre),
+    [],
+  );
 
 const filterPlaylistCategory = (playlist: Playlist, filter: string) => {
-    if (!filter) return playlist
+  if (!filter) return playlist;
 
-    return playlist.filter(({ genre }) => genre === filter)
+  return playlist.filter(({ genre }) => genre === filter);
+};
 
-}
+const chunk = <T>(input: T[], size: number) => {
+  return input?.reduce((arr: T[][], item, idx: number) => {
+    return idx % size === 0
+      ? [...arr, [item]]
+      : [...arr.slice(0, -1), [...arr.slice(-1)[0], item]];
+  }, []);
+};
 
-export { getCategoriesFromPlaylist, filterPlaylistCategory }
+export { getCategoriesFromPlaylist, filterPlaylistCategory, chunk };
