3a403b8e33f4aa261df9208e90da3227d361020a
Robin van Zanten <38441984+RCVZ@users.noreply.github.com>
2021-05-07 14:23:11 +0200

    chore(project): fix playlist screen

--- src/screens/Playlist/Playlist.tsx
+++ src/screens/Playlist/Playlist.tsx
@@ -1,26 +1,30 @@
 import React, { useState } from 'react';
+import { useParams } from 'react-router-dom';
+import type { GridCellProps } from 'react-virtualized';
 
+import VirtualizedGrid from '../../components/VirtualizedGrid/VirtualizedGrid';
+import Layout from '../../components/Layout/Layout';
 import usePlaylist from '../../hooks/usePlaylist';
-import { getCategoriesFromPlaylist, filterPlaylistCategory } from '../../utils/collection';
+import { getCategoriesFromPlaylist, filterPlaylistCategory, chunk } from '../../utils/collection';
 import Card from '../../components/Card/Card';
-import Dropdown from '../../components/Dropdown/Dropdown';
-import CardGrid from '../../components/CardGrid/CardGrid';
+import Dropdown from '../../components/Filter/Filter';
+import useBreakpoint, { Breakpoint } from '../../hooks/useBreakpoint';
 
 import styles from './Playlist.module.scss';
 
-// TEMP DATA
-const playlistId = 'sR5VypYk';
-
-type PlaylistDestructuredArguments = {
-  mediaid: string;
-  title: string;
-  duration: number;
-  image: string;
+const cols = {
+  [Breakpoint.xs]: 2,
+  [Breakpoint.sm]: 2,
+  [Breakpoint.md]: 2,
+  [Breakpoint.lg]: 4,
+  [Breakpoint.xl]: 5,
 };
 
 function Playlist() {
-  const { isLoading, error, data: { title, playlist } = {} } = usePlaylist(playlistId);
+  const breakpoint: Breakpoint = useBreakpoint();
+  const { id: playlistId } = useParams<Record<string, string>>();
   const [filter, setFilter] = useState<string>('');
+  const { isLoading, error, data: { title, playlist } = {} } = usePlaylist(playlistId);
 
   if (isLoading) return <p>Loading...</p>;
 
@@ -28,6 +32,27 @@ function Playlist() {
 
   const categories = getCategoriesFromPlaylist(playlist);
   const filteredPlaylist = filterPlaylistCategory(playlist, filter);
+
+  const playlistRows = chunk(filteredPlaylist, cols[breakpoint]);
+
+  const cellRenderer = ({ columnIndex, key, rowIndex, style }: GridCellProps) => {
+    if (!playlistRows[rowIndex][columnIndex]) return;
+
+    const { mediaid: mediaId, title, duration, image, seriesId } = playlistRows[rowIndex][columnIndex];
+
+    return (
+      <div className={styles.wrapper} style={style} key={key}>
+        <Card
+          key={mediaId}
+          title={title}
+          duration={duration}
+          posterSource={image}
+          seriesId={seriesId}
+          onClick={() => ''}
+        />
+      </div>
+    );
+  };
 
   return (
     <div className={styles.playlist}>
@@ -38,11 +63,7 @@ function Playlist() {
         )}
       </header>
       <main>
-        <CardGrid>
-          {filteredPlaylist.map(({ mediaid: mediaId, title, duration, image }: PlaylistDestructuredArguments) => (
-            <Card key={mediaId} title={title} duration={duration} posterSource={image} onClick={() => ''} />
-          ))}
-        </CardGrid>
+        <VirtualizedGrid rowCount={playlistRows.length} cellRenderer={cellRenderer} spacing={30} />
       </main>
     </div>
   );
