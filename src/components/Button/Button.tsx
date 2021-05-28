a1302d2563de15c0b5680c92d7d703587f97ddf3
Roy Schut <roy@videodock.com>
2021-05-28 12:14:32 +0200

    feat(project): add styling and icon props to button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -3,19 +3,34 @@ import classNames from 'classnames';
 
 import styles from './Button.module.scss';
 
+type Color = 'primary' | 'secondary';
+
 type Props = {
   label: string;
-  active: boolean;
+  active?: boolean;
+  color?: Color;
+  fullWidth?: boolean;
+  startIcon?: JSX.Element;
   onClick: () => void;
 };
-const Button: React.FC<Props> = ({ label, active, onClick }: Props) => {
+const Button: React.FC<Props> = ({
+  label,
+  color = 'primary',
+  startIcon,
+  fullWidth = false,
+  active = false,
+  onClick,
+}: Props) => {
   return (
     <button
       className={classNames(styles.button, {
         [styles.active]: active,
+        [styles.secondary]: color === 'secondary',
+        [styles.fullWidth]: fullWidth,
       })}
       onClick={onClick}
     >
+      {startIcon ? <div className={styles.startIcon}>{startIcon}</div> : null}
       <span className={styles.buttonLabel}>{label}</span>
     </button>
   );
