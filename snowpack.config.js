4a2883fc6ac3d58e6e8465662a355319e0fd737a
Christiaan Scheermeijer <christiaan@videodock.com>
2021-06-03 16:37:51 +0200

    fix(project): fix snowpack optimize using webpack

--- snowpack.config.js
+++ snowpack.config.js
@@ -15,33 +15,33 @@ module.exports = {
     '@snowpack/plugin-react-refresh',
     '@snowpack/plugin-dotenv',
     '@snowpack/plugin-sass',
-    // ['@snowpack/plugin-webpack', {
-    //   extendConfig: (config) => {
-    //     // FIXES https://github.com/snowpackjs/snowpack/discussions/2810
-    //     config.module.rules.find(
-    //       (rule) =>
-    //         rule &&
-    //         rule.use &&
-    //         rule.use.find((use) => {
-    //           if (
-    //             !use ||
-    //             !use.loader ||
-    //             !use.loader.includes('babel-loader')
-    //           ) {
-    //             return null;
-    //           }
-    //
-    //           use.options.plugins = (use.options.plugins || []).concat([
-    //             '@babel/plugin-proposal-optional-chaining',
-    //             '@babel/plugin-proposal-nullish-coalescing-operator'
-    //           ]);
-    //
-    //           return use;
-    //         }),
-    //     );
-    //     return config;
-    //   },
-    // }],
+    ['@snowpack/plugin-webpack', {
+      extendConfig: (config) => {
+        // FIXES https://github.com/snowpackjs/snowpack/discussions/2810
+        const babelRule = config.module.rules.find((rule) =>
+          rule && rule.use && rule.use.find((use) => use && use.loader && use.loader.includes('babel-loader')));
+
+        if (babelRule) {
+          babelRule.use = babelRule.use.filter(use => {
+            if (use.loader.includes('babel-loader')) {
+              use.options.plugins = (use.options.plugins || []).concat([
+                '@babel/plugin-proposal-optional-chaining',
+                '@babel/plugin-proposal-nullish-coalescing-operator'
+              ]);
+            }
+
+            // filter out proxy-import-resolve
+            if (use.loader.includes('proxy-import-resolve')) {
+              return false;
+            }
+
+            return true;
+          });
+        }
+
+        return config;
+      },
+    }],
     [
       '@snowpack/plugin-typescript',
       {
