2e2f4425ff4d100a7fe43a80a9ebdeb37eba8af8
Roy Schut <roy@videodock.com>
2021-06-04 15:01:33 +0200

    chore(videodetail): move fill color to css

--- src/icons/Play.tsx
+++ src/icons/Play.tsx
@@ -5,6 +5,6 @@ import createIcon from './Icon';
 export default createIcon(
   '0 0 24 24',
   <g>
-    <path fill="currentColor" d="M8,5.14V19.14L19,12.14L8,5.14Z" />
+    <path d="M8,5.14V19.14L19,12.14L8,5.14Z" />
   </g>,
 );
