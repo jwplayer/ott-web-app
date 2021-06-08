a62dff13806181d3f15b22cb44041c01f350ed2e
Christiaan Scheermeijer <christiaan@videodock.com>
2021-06-08 15:11:08 +0200

    feat(project): initialize CodeceptJS tests

---
+++ codecept.desktop.js
@@ -0,0 +1,37 @@
+const { setHeadlessWhen } = require('@codeceptjs/configure');
+
+// turn on headless mode when running with HEADLESS=true environment variable
+// export HEADLESS=true && npx codeceptjs run
+setHeadlessWhen(process.env.HEADLESS);
+
+exports.config = {
+  grep     : '@desktop',
+  tests    : './test/*_test.js',
+  output   : './output',
+  helpers  : {
+    Playwright: {
+      url    : 'http://localhost:8080',
+      show   : false,
+      browser: 'chromium',
+    }
+  },
+  include  : {
+    I: './steps_file.js'
+  },
+  bootstrap: null,
+  mocha    : {},
+  name     : 'desktop',
+  plugins  : {
+    pauseOnFail     : {},
+    retryFailedStep : {
+      enabled: true,
+      retries: 2,
+    },
+    tryTo           : {
+      enabled: true
+    },
+    screenshotOnFail: {
+      enabled: !process.env.CI
+    }
+  }
+};
