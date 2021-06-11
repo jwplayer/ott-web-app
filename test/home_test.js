91a9f4d4e66e1b2697d0f8d4df84f1c995638c97
Christiaan Scheermeijer <christiaan@videodock.com>
2021-06-11 08:58:20 +0200

    test(search): add e2e tests for search

--- test/home_test.js
+++ test/home_test.js
@@ -1,18 +1,11 @@
-Feature('home');
+Feature('home').tag('@desktop');
 
-Scenario('Homepage loads', ({ I }) => {
+Scenario('Desktop home screen loads', ({ I }) => {
   I.amOnPage('http://localhost:8080');
   I.see('Central Intelligence');
   I.see('All Movies');
   I.see('Settings');
-}).tag('@desktop');
-
-Scenario('Homepage loads', ({ I }) => {
-  I.amOnPage('http://localhost:8080');
-  I.see('Central Intelligence');
-  I.see('Drama');
-  I.see('The Spongebob Movie');
-}).tag('@mobile');
+});
 
 Scenario('Header button navigates to playlist screen', ({ I }) => {
   I.amOnPage('http://localhost:8080');
@@ -20,15 +13,4 @@ Scenario('Header button navigates to playlist screen', ({ I }) => {
   I.click('All Movies');
   I.amOnPage('http://localhost:8080/p/4fgzPjpv');
   I.see('Featured Covers');
-}).tag('@desktop');
-
-Scenario('Menu button opens the sidebar', ({ I }) => {
-  I.amOnPage('http://localhost:8080');
-  I.click('[aria-label="Open menu"]');
-  I.see('Home');
-  I.see('All Movies');
-  I.see('Settings');
-  I.click('All Movies');
-  I.amOnPage('http://localhost:8080/p/4fgzPjpv');
-  I.see('Featured Covers');
-}).tag('@mobile');
+});
