7ab705a36c3cb233fc01ea76469f88fe0b8291b7
Roy Schut <roy@videodock.com>
2021-06-17 14:41:09 +0200

    feat(project): skip filtering when only 1 item

--- src/components/Filter/Filter.tsx
+++ src/components/Filter/Filter.tsx
@@ -20,10 +20,9 @@ const Filter: FC<Props> = ({ name, value, defaultLabel, options, setValue }) => 
   const [isFilterModalOpen, openFilterModal] = useState(false);
   const breakpoint: Breakpoint = useBreakpoint();
 
-  if (!options.length) {
+  if (options.length < 2) {
     return null;
   }
-
   const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => setValue(event.target.value);
 
   const handleOnClick = () => {
