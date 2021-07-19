df27e8ec2f24e945572ee8904da943dfe08327a5
Robin van Zanten <robin@videodock.com>
2021-07-19 11:20:12 +0200

    feat(project): add width and height to logo img

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Link } from 'react-router-dom';
 
 import styles from './Logo.module.scss';
@@ -8,10 +8,31 @@ type Props = {
   onLoad: () => void;
 };
 
+type ImgRef = {
+  height?: number;
+  width?: number;
+};
+
 const Logo: React.FC<Props> = ({ src, onLoad }: Props) => {
+  const imgRef = useRef<ImgRef>({ height: undefined, width: undefined });
+
+  const onLoadHandler = (event: React.SyntheticEvent<HTMLImageElement, Event>) => {
+    const { height, width } = event.currentTarget;
+    imgRef.current = { height, width };
+    onLoad();
+  };
+
   return (
     <Link to="/">
-      <img className={styles.logo} alt="logo" src={src} onLoad={onLoad} onError={onLoad} />
+      <img
+        className={styles.logo}
+        alt="logo"
+        src={src}
+        height={imgRef.current.height}
+        width={imgRef.current.width}
+        onLoad={onLoadHandler}
+        onError={onLoad}
+      />
     </Link>
   );
 };
