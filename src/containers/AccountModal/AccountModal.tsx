dbdb875c9c3f508f72985069927e88abd53fcef3
Christiaan Scheermeijer <christiaan@videodock.com>
2021-07-21 11:37:40 +0200

    feat(auth): add LoginForm component

--- src/containers/AccountModal/AccountModal.tsx
+++ src/containers/AccountModal/AccountModal.tsx
@@ -1,11 +1,11 @@
 import React, { useContext } from 'react';
 import { useHistory } from 'react-router';
 
-import Button from '../../components/Button/Button';
 import { ConfigContext } from '../../providers/ConfigProvider';
 import Dialog from '../../components/Dialog/Dialog';
 import useQueryParam from '../../hooks/useQueryParam';
 import { removeQueryParam } from '../../utils/history';
+import LoginForm from '../../components/LoginForm/LoginForm';
 
 import styles from './AccountModal.module.scss';
 
@@ -24,9 +24,7 @@ const AccountModal = () => {
   return (
     <Dialog open={!!view} onClose={closeHandler}>
       <div className={styles.banner}>{banner ? <img src={banner} alt="" /> : null}</div>
-      <h2 className={styles.title}>Login!</h2>
-      <form>form</form>
-      <Button label="Sign in" variant="contained" color="primary" fullWidth />
+      <LoginForm onSubmit={(event, formData) => console.info(event, formData)} />
     </Dialog>
   );
 };
