23e2ef3db38a800c95dd71b0eb1acb8b0d1f12b4
Robin van Zanten <robin@videodock.com>
2021-07-27 14:42:25 +0200

    feat(project): add header to checkbox

--- src/components/Checkbox/Checkbox.tsx
+++ src/components/Checkbox/Checkbox.tsx
@@ -1,21 +1,23 @@
-import React, { ReactNode } from 'react';
+import React from 'react';
 
 import useOpaqueId from '../../hooks/useOpaqueId';
 
 import styles from './Checkbox.module.scss';
 
 type Props = {
-  label?: string | ReactNode;
+  label?: string;
   name: string;
   value: boolean;
   onChange: React.ChangeEventHandler<HTMLInputElement>;
+  header?: string;
 };
 
-const Checkbox: React.FC<Props> = ({ label, name, onChange }: Props) => {
+const Checkbox: React.FC<Props> = ({ label, name, onChange, header }: Props) => {
   const id = useOpaqueId('check-box', name);
 
   return (
     <div className={styles.checkbox}>
+      {header && <span className={styles.header}>{header}</span>}
       <input name={name} type="checkbox" id={id} onChange={onChange} />
       <label htmlFor={id}>{label}</label>
     </div>
