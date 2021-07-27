791f4189a2cfb9c219743e2e5f73ccf2bfd491ed
Robin van Zanten <robin@videodock.com>
2021-07-27 14:44:23 +0200

    chore(project): refactor dropdown component

--- src/components/Dropdown/Dropdown.tsx
+++ src/components/Dropdown/Dropdown.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import classNames from 'classnames';
 
+import useOpaqueId from '../../hooks/useOpaqueId';
+
 import styles from './Dropdown.module.scss';
 
 type Props = {
   name: string;
   value: string;
-  defaultLabel: string;
-  options: string[];
+  defaultLabel?: string;
+  options?: string[];
   optionsStyle?: string;
   valuePrefix?: string;
-  onChange: (event: React.ChangeEvent<HTMLSelectElement>) => void;
+  label?: string;
+  fullWidth?: boolean;
+  onChange: React.ChangeEventHandler;
 };
 
 const Dropdown: React.FC<Props & React.AriaAttributes> = ({
@@ -20,23 +24,36 @@ const Dropdown: React.FC<Props & React.AriaAttributes> = ({
   options,
   onChange,
   optionsStyle,
+  label,
+  fullWidth,
   valuePrefix,
   ...rest
 }: Props & React.AriaAttributes) => {
+  const id = useOpaqueId();
   return (
-    <div className={styles.dropdown}>
-      <select className={styles.select} name={name} value={value} onChange={onChange} {...rest}>
-        <option className={classNames(styles.option, optionsStyle)} value="">
-          {defaultLabel}
-        </option>
-        {options.map((option) => (
-          <option className={classNames(styles.option, optionsStyle)} key={option} value={option}>
-            {valuePrefix}
-            {option}
-          </option>
-        ))}
-      </select>
-      <span className="focus" />
+    <div className={classNames(styles.container, { [styles.fullWidth]: fullWidth })}>
+      {label && (
+        <label htmlFor={id} className={styles.label}>
+          {label}
+        </label>
+      )}
+      <div className={classNames(styles.dropdown, { [styles.fullWidth]: fullWidth })}>
+        <select id={id} className={styles.select} name={name} value={value} onChange={onChange} {...rest}>
+          {defaultLabel && (
+            <option className={classNames(styles.option, optionsStyle)} value="">
+              {defaultLabel}
+            </option>
+          )}
+          {options &&
+            options.map((option) => (
+              <option className={classNames(styles.option, optionsStyle)} key={option} value={option}>
+                {valuePrefix}
+                {option}
+              </option>
+            ))}
+        </select>
+        <span className="focus" />
+      </div>
     </div>
   );
 };
