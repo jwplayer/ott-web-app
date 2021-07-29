c0884d79df71993152eb5d4f71eb4c3fd8b00c71
Robin van Zanten <robin@videodock.com>
2021-07-29 14:56:15 +0200

    chore(project): tweaked styling checkbox

--- src/components/Checkbox/Checkbox.tsx
+++ src/components/Checkbox/Checkbox.tsx
@@ -1,26 +1,36 @@
 import React from 'react';
+import classNames from 'classnames';
 
 import useOpaqueId from '../../hooks/useOpaqueId';
 
 import styles from './Checkbox.module.scss';
 
 type Props = {
-  label?: string;
+  label?: string | JSX.Element;
   name: string;
-  value: string;
+  value?: string;
   checked?: boolean;
   onChange: React.ChangeEventHandler<HTMLInputElement>;
   header?: string;
+  error?: boolean;
+  helperText?: string;
+  disabled?: boolean;
 };
 
-const Checkbox: React.FC<Props> = ({ label, name, onChange, header, checked }: Props) => {
+const Checkbox: React.FC<Props> = ({ label, name, onChange, header, checked, value, helperText, disabled, error }: Props) => {
+  const checkboxClassName = classNames(styles.checkbox, {
+    [styles.error]: error,
+  });
   const id = useOpaqueId('check-box', name);
 
+  const labelComponent = typeof label === 'string' ? <label htmlFor={id}>{label}</label> : label && React.cloneElement(label, { htmlFor: id });
+
   return (
-    <div className={styles.checkbox}>
+    <div className={checkboxClassName}>
       {header && <span className={styles.header}>{header}</span>}
-      <input name={name} type="checkbox" id={id} onChange={onChange} checked={checked} />
-      <label htmlFor={id}>{label}</label>
+      <input name={name} type="checkbox" id={id} onChange={onChange} value={value} checked={checked} disabled={disabled} />
+      {labelComponent}
+      {helperText ? <div className={styles.helperText}>{helperText}</div> : null}
     </div>
   );
 };
