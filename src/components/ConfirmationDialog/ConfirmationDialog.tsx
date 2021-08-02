d1291a4c382e97bc5fde08388bcc7c7df3581761
Christiaan Scheermeijer <christiaan@videodock.com>
2021-08-02 15:45:17 +0200

    chore(project): format code

--- src/components/ConfirmationDialog/ConfirmationDialog.tsx
+++ src/components/ConfirmationDialog/ConfirmationDialog.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 
 import Dialog from '../Dialog/Dialog';
 import Button from '../Button/Button';
 
 import styles from './ConfirmationDialog.module.scss';
-import { useTranslation } from 'react-i18next';
 
 type Props = {
   open: boolean;
@@ -21,7 +21,14 @@ const ConfirmationDialog: React.FC<Props> = ({ open, title, body, onConfirm, onC
     <Dialog open={open} onClose={onClose}>
       <h2 className={styles.title}>{title}</h2>
       <p className={styles.body}>{body}</p>
-      <Button className={styles.confirmButton} label={t('confirmation_dialog.confirm')} variant="contained" color="primary" onClick={onConfirm} fullWidth />
+      <Button
+        className={styles.confirmButton}
+        label={t('confirmation_dialog.confirm')}
+        variant="contained"
+        color="primary"
+        onClick={onConfirm}
+        fullWidth
+      />
       <Button label={t('confirmation_dialog.close')} variant="outlined" onClick={onClose} fullWidth />
     </Dialog>
   );
