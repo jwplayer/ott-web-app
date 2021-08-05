5af0a1e913d84f46cc2dd31c745768024b6e97a0
Christiaan Scheermeijer <christiaan@videodock.com>
2021-08-05 16:36:49 +0200

    refactor(player): refactor player events

--- types/jwplayer.d.ts
+++ types/jwplayer.d.ts
@@ -1,3 +1,4 @@
 export type JWPlayer = jwplayer.JWPlayer & {
   on(event: 'userActive' | 'userInactive', callback: () => void);
+  off(event: 'userActive' | 'userInactive', callback: () => void);
 };
