aea3a21fb6b5c88c1d70fe77901e8ca297c707b6
Christiaan Scheermeijer <christiaan@videodock.com>
2021-08-05 14:21:44 +0200

    chore(project): remove relative link

--- src/screens/About/About.tsx
+++ src/screens/About/About.tsx
@@ -18,7 +18,7 @@ To see an example of JW OTT Webapp in action, see [https://jw-ott-webapp.netlify
 - Video titles, descriptions and hero images are populated from JW Platform JSON feed metadata.
 - Playback of HLS video content from the JW Platform CDN. You can add external URLs (for example, URLS from your own server or CDN) to your playlists in the Content section of your JW Player account dashboard, but they must be HLS streams (\`.m3u8\` files).
 - Support for live video streams (must be registered as external .m3u8 URLs in your JW Dashboard).
-- Customize the user interface with your own branding. The default app is configured for JW Player branding and content, but you can easily change this to use your own assets by modifying the \`config.json\` file. Advanced customization is possible (for example, editing the CSS files), but you will need to modify the source code and [build from source](docs/build-from-source.md).
+- Customize the user interface with your own branding. The default app is configured for JW Player branding and content, but you can easily change this to use your own assets by modifying the \`config.json\` file. Advanced customization is possible (for example, editing the CSS files), but you will need to modify the source code and build from source.
 - Site-wide video search and related video recommendations powered by [JW Recommendations](https://support.jwplayer.com/customer/portal/articles/2191721-jw-recommendations).
 - Basic playback analytics is reported to your JW Dashboard.
 - Ad integrations (VAST, VPAID, GoogleIMA, etc.). These features require a JW Player Ads Edition license. For more information, see the [JW Player pricing page](https://www.jwplayer.com/pricing/).
