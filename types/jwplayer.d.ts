3e7d7b4c6157d598b2e52c9d5534de67ed2f1eb9
Roy Schut <roy@videodock.com>
2021-08-06 14:31:09 +0200

    fix(player): fix autoplay

--- types/jwplayer.d.ts
+++ types/jwplayer.d.ts
@@ -1,4 +1,5 @@
 export type JWPlayer = jwplayer.JWPlayer & {
   on(event: 'userActive' | 'userInactive', callback: () => void);
   off(event: 'userActive' | 'userInactive', callback: () => void);
+  setConfig({ playlist, autostart });
 };
