ef2db820bd726ad81f92fefb4b2ed2f3be5758dc
Christiaan Scheermeijer <christiaan@videodock.com>
2022-05-17 14:18:44 +0200

    chore(project): include Zustand devtools only for dev env

--- src/stores/utils.ts
+++ src/stores/utils.ts
@@ -2,10 +2,19 @@ import type { State, StateCreator } from 'zustand';
 import create from 'zustand';
 import { devtools, subscribeWithSelector } from 'zustand/middleware';
 
-export const createStore = <T extends State>(name: string, storeFn: StateCreator<T>) =>
-  create(
-    devtools(subscribeWithSelector(storeFn), {
-      name,
-      anonymousActionType: `${name}/Action`,
-    }),
-  );
+import { IS_DEV_BUILD } from '#src/utils/common';
+
+export const createStore = <T extends State>(name: string, storeFn: StateCreator<T>) => {
+  const store = subscribeWithSelector(storeFn);
+
+  if (IS_DEV_BUILD) {
+    return create(
+      devtools(store, {
+        name,
+        anonymousActionType: `${name}/Action`,
+      }),
+    );
+  }
+
+  return create(store);
+};
