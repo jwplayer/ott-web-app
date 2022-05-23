1bc7d6b5e94fc2032ffe08eb75653bdf806efb5a
Christiaan Scheermeijer <christiaan@videodock.com>
2022-05-23 22:49:58 +0200

    chore: react query staleTime still wrong value

--- src/providers/QueryProvider.tsx
+++ src/providers/QueryProvider.tsx
@@ -4,7 +4,7 @@ import { QueryClient, QueryClientProvider } from 'react-query';
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      staleTime: 60 * 60 * 3600,
+      staleTime: 60 * 60 * 1000,
       refetchOnWindowFocus: false,
       retryOnMount: false,
     },
