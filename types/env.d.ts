de6d497afe44a9b46683d296032afb9fb0414efb
Danny Budzinski <dbudzins@users.noreply.github.com>
2022-07-07 11:09:52 +0200

    Update types/env.d.ts
    Co-authored-by: Christiaan Scheermeijer <christiaan@videodock.com>

--- types/env.d.ts
+++ types/env.d.ts
@@ -5,7 +5,7 @@ interface ImportMetaEnv {
   readonly APP_GITHUB_PUBLIC_BASE_URL: string;
   readonly APP_CONFIG_DEFAULT_SOURCE: string;
   readonly APP_CONFIG_ALLOWED_SOURCES: string | undefined;
-  readonly APP_UNSAFE_ALLOW_DYNAMIC_CONFIG: boolean | undefined;
+  readonly APP_UNSAFE_ALLOW_DYNAMIC_CONFIG: string | undefined;
 }
 
 interface ImportMeta {
