7a607c27be85f8bcb916173165df617b480a6d56
“Anton <alantukh@jwplayer.com>
2022-07-13 09:54:06 +0200

    chore(project): review fixes for dev utility

--- src/components/ConfigSelect/index.tsx
+++ src/components/ConfigSelect/index.tsx
@@ -1,24 +1,27 @@
-import { useTranslation } from 'react-i18next';
-
 import Dropdown from '../Dropdown/Dropdown';
 
 import styles from './ConfigSelect.module.scss';
 
 import { getStoredConfig, setStoredConfig } from '#src/utils/configOverride';
+import { logDev } from '#src/utils/common';
 
 const configs = import.meta.glob('../../../test-e2e/data/*.json');
 
 const configOptions = Object.entries(configs).reduce<string[]>((acc, [pth]) => {
-  const parts = pth.split('/');
-  const fileName = parts.pop();
-  const configName = fileName?.split('.')[1] as string;
+  let configName = '';
 
-  return [...acc, configName];
+  try {
+    const parts = pth.split('/');
+    const fileName = parts.pop();
+    configName = fileName?.split('.')[1] as string;
+  } catch (err: unknown) {
+    logDev('Config file could not be processed: ' + err);
+  }
+
+  return configName ? [...acc, configName] : acc;
 }, []);
 
 const ConfigSelect = () => {
-  const { t } = useTranslation('common');
-
   const selectedConfig = getStoredConfig() || '';
 
   const onChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
@@ -35,7 +38,6 @@ const ConfigSelect = () => {
       name="config-select"
       value={selectedConfig}
       onChange={onChange}
-      aria-label={t('select_config')}
     />
   );
 };
