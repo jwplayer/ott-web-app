712b157edc552e8d0a6ec0d3727d54b1bd8b5e98
Robin van Zanten <rcvzanten@gmail.com>
2022-07-28 14:10:20 +0200

    feat(epg): add ended state to epgprogram

--- src/components/EpgProgramItem/EpgProgramItem.tsx
+++ src/components/EpgProgramItem/EpgProgramItem.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Program, useProgram } from 'planby';
 import classNames from 'classnames';
 import { useTranslation } from 'react-i18next';
+import { isPast } from 'date-fns';
 
 import styles from './EpgProgramItem.module.scss';
 
@@ -27,6 +28,7 @@ const ProgramItem: React.VFC<Props> = ({ program, onClick, isActive, compact }) 
   const { t } = useTranslation('common');
   const { data } = program;
   const { image, title, since, till } = data;
+  const ended = isPast(new Date(till));
 
   const sinceTime = formatTime(since, set12HoursTimeFormat()).toLowerCase();
   const tillTime = formatTime(till, set12HoursTimeFormat()).toLowerCase();
@@ -36,7 +38,10 @@ const ProgramItem: React.VFC<Props> = ({ program, onClick, isActive, compact }) 
 
   return (
     <div className={styles.epgProgramBox} style={position} onClick={() => onClick && onClick(program)}>
-      <div className={classNames(styles.epgProgram, { [styles.selected]: isActive, [styles.live]: isLive })} style={{ width: styles.width }}>
+      <div
+        className={classNames(styles.epgProgram, { [styles.selected]: isActive, [styles.live]: isLive, [styles.ended]: ended })}
+        style={{ width: styles.width }}
+      >
         {showImage && <img className={styles.epgProgramImage} src={image} alt="Preview" />}
         {showLiveTagInImage && <div className={styles.epgLiveTag}>{t('live')}</div>}
         <div className={styles.epgProgramContent}>
