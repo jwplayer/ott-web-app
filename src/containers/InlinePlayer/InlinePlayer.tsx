0426459a8c7cd70fd96537708c5f4091922d348a
Robin van Zanten <rcvzanten@gmail.com>
2022-09-28 14:16:27 +0200

    feat(inlineplayer): add public overlay

--- src/containers/InlinePlayer/InlinePlayer.tsx
+++ src/containers/InlinePlayer/InlinePlayer.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import { useLocation, useNavigate } from 'react-router';
+import { useTranslation } from 'react-i18next';
 
 import PlayerContainer from '../PlayerContainer/PlayerContainer';
 
 import styles from './InlinePlayer.module.scss';
 
+import Image from '#src/components/Image/Image';
 import type { PlaylistItem } from '#types/playlist';
+import Fade from '#src/components/Animation/Fade/Fade';
+import Lock from '#src/icons/Lock';
+import Button from '#src/components/Button/Button';
+import { addQueryParam } from '#src/utils/location';
+import { useConfigStore } from '#src/stores/ConfigStore';
 
 type Props = {
   open: boolean;
@@ -16,22 +24,57 @@ type Props = {
   liveStartDateTime?: string | null;
   liveEndDateTime?: string | null;
   liveFromBeginning?: boolean;
+  startWatchingButton: React.ReactNode;
+  isLocked: boolean;
 };
 
-const InlinePlayer: React.FC<Props> = ({ open, item, onPlay, onPause, onComplete, feedId, liveStartDateTime, liveEndDateTime, liveFromBeginning }: Props) => {
+const InlinePlayer: React.FC<Props> = ({
+  open,
+  item,
+  onPlay,
+  onPause,
+  onComplete,
+  feedId,
+  liveStartDateTime,
+  liveEndDateTime,
+  liveFromBeginning,
+  startWatchingButton,
+  isLocked,
+}: Props) => {
+  const siteName = useConfigStore((s) => s.config.siteName);
+  const { t } = useTranslation();
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const loginButtonClickHandler = () => {
+    navigate(addQueryParam(location, 'u', 'login'));
+  };
+
   return (
     <div className={styles.inlinePlayer}>
-      <PlayerContainer
-        visible={open}
-        item={item}
-        feedId={feedId}
-        onPlay={onPlay}
-        onPause={onPause}
-        onComplete={onComplete}
-        liveEndDateTime={liveEndDateTime}
-        liveFromBeginning={liveFromBeginning}
-        liveStartDateTime={liveStartDateTime}
-      />
+      <Fade open={isLocked}>
+        <div className={styles.lockedOverlay}>
+          <Image className={styles.poster} image={item.shelfImage} alt={item.title} width={1280} />
+          <Lock className={styles.lock} />
+          <h2 className={styles.callToAction}>{t('video:sign_up_to_start_watching')}</h2>
+          <span className={styles.text}>{t('account:choose_offer.watch_this_on_platform', { siteName })}</span>
+          {startWatchingButton}
+          <Button onClick={loginButtonClickHandler} label={t('common:sign_in')} />
+        </div>
+      </Fade>
+      {!isLocked && (
+        <PlayerContainer
+          visible={open}
+          item={item}
+          feedId={feedId}
+          onPlay={onPlay}
+          onPause={onPause}
+          onComplete={onComplete}
+          liveEndDateTime={liveEndDateTime}
+          liveFromBeginning={liveFromBeginning}
+          liveStartDateTime={liveStartDateTime}
+        />
+      )}
     </div>
   );
 };
