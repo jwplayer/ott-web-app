7a40e691d5ff05a81a3d51778874e1b53d9f849e
Danny Budzinski <dbudzinski@jwplayer.com>
2022-11-08 23:14:57 +0100

    chore: clean test logs and fix skipped tests

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -53,7 +53,7 @@ export const IS_DEV_BUILD = import.meta.env.DEV;
 export const IS_TEST_BUILD = import.meta.env.MODE === 'test';
 
 export function logDev(message: unknown, ...optionalParams: unknown[]) {
-  if (IS_DEV_BUILD) {
+  if (IS_DEV_BUILD && !IS_TEST_MODE) {
     if (optionalParams.length > 0) {
       console.info(message, optionalParams);
     } else {
