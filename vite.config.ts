59306b867a22dbf62051db39752d29aef6616f2d
Danny Budzinski <dbudzinski@jwplayer.com>
2022-11-23 23:26:23 +0100

    fix: figured out the mode shortening to dev/prod

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,11 @@ import { createHtmlPlugin } from 'vite-plugin-html';
 import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default ({ mode, command }: { mode: string; command: string }) => {
+  // Shorten default mode names to dev / prod
+  // Also differentiates from build type (production / development)
+  mode = mode === 'development' ? 'dev' : mode;
+  mode = mode === 'production' ? 'prod' : mode;
+
   // Make sure to builds are always production type,
   // otherwise modes other than 'production' get built in dev
   if (command === 'build') {
@@ -26,7 +31,7 @@ export default ({ mode, command }: { mode: string; command: string }) => {
   ];
 
   // These files are only needed in dev / test / demo, so don't include in prod builds
-  if (mode !== 'prod' && mode !== 'production') {
+  if (mode !== 'prod') {
     plugins.push(
       viteStaticCopy({
         targets: [
@@ -46,6 +51,7 @@ export default ({ mode, command }: { mode: string; command: string }) => {
     server: {
       port: 8080,
     },
+    mode: mode,
     build: {
       outDir: './build/public',
       cssCodeSplit: false,
