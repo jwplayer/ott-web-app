2789396676bb537f6fdaa4df3a442e3bf603499f
Danny Budzinski <dbudzinski@jwplayer.com>
2022-11-24 14:52:12 +0100

    chore: add test ids to demo mode for debug

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -56,8 +56,7 @@ export const IS_DEMO_MODE = import.meta.env.MODE === 'demo';
 export const IS_TEST_MODE = import.meta.env.MODE === 'test';
 
 export function logDev(message: unknown, ...optionalParams: unknown[]) {
-  // TODO: Remove IS_DEMO_MODE
-  if (IS_DEMO_MODE || (IS_DEV_BUILD && !IS_TEST_MODE)) {
+  if (IS_DEV_BUILD && !IS_TEST_MODE) {
     if (optionalParams.length > 0) {
       console.info(message, optionalParams);
     } else {
@@ -75,5 +74,6 @@ export function getOverrideIP() {
 }
 
 export function testId(value: string | undefined) {
-  return IS_DEV_BUILD || IS_TEST_MODE ? value : undefined;
+  // TODO: Remove IS_DEMO_MODE
+  return IS_DEMO_MODE || IS_DEV_BUILD || IS_TEST_MODE ? value : undefined;
 }
