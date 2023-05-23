e34afe4b2bd0af0393d28fd0639f827961945ace
Carina Dragan <carinadragan10@gmail.com>
2023-05-23 15:32:47 +0200

    feat: code cleanup

--- types/jwpltx.d.ts
+++ types/jwpltx.d.ts
@@ -1,5 +1,5 @@
 interface Jwpltx {
-  ready: (analyticsId: string, hostname: string, feedid: string, mediaid: string, title: string, appAccountId?: number) => void;
+  ready: (analyticsid: string, hostname: string, feedid: string, mediaid: string, title: string, accountid?: number) => void;
   adImpression: () => void;
   seek: (offset: number, duration: number) => void;
   seeked: () => void;
