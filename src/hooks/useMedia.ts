b62300f71b8d2c2e28df997466d0f2d6a1038d3b
Robin <rcvzanten@gmail.com>
2023-05-31 15:13:48 +0200

    feat(project): auto refetch media item when it is event media item

--- src/hooks/useMedia.ts
+++ src/hooks/useMedia.ts
@@ -2,11 +2,19 @@ import { UseBaseQueryResult, useQuery } from 'react-query';
 
 import { getMediaById } from '#src/services/api.service';
 import type { PlaylistItem } from '#types/playlist';
+import { isScheduledOrLiveMedia } from '#src/utils/liveEvent';
 
 export type UseMediaResult<TData = PlaylistItem, TError = unknown> = UseBaseQueryResult<TData, TError>;
 
 export default function useMedia(mediaId: string, enabled: boolean = true): UseMediaResult {
   return useQuery(['media', mediaId], () => getMediaById(mediaId), {
     enabled: !!mediaId && enabled,
+    refetchInterval: (data, _) => {
+      if (!data) return false;
+
+      const autoRefetch = isScheduledOrLiveMedia(data);
+
+      return autoRefetch ? 1000 * 30 : false;
+    },
   });
 }
