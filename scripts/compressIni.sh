12190fe7b957ea79f8a567390cbbea8ccf7cdcc2
Danny Budzinski <dbudzinski@jwplayer.com>
2023-06-01 20:58:42 +0200

    chore: mac compatible compress ini script

--- scripts/compressIni.sh
+++ scripts/compressIni.sh
@@ -1,9 +1,11 @@
 # This script cleans up the ini file to remove comments and whitespace, so it only keeps the actual data
 # Remove lines that are solely comments with optional leading whitespace
-sed -i -e '/^[[:blank:]]*;/d' "$1"
+sed '/^[[:blank:]]*;/d' "$1" |
 # Remove any comments that come after values
-sed -i -e 's/;.*//' "$1"
+sed 's/;.*//' |
 # Delete blank lines
-sed -i -e '/^$/d' "$1"
+sed '/^$/d' |
 # Remove whitespace around the equals signs
-sed -i -e 's/[[:blank:]]*=[[:blank:]]/=/' "$1"
+sed 's/[[:blank:]]*=[[:blank:]]/=/' > "$1.tmp.ini"
+
+mv "$1.tmp.ini" "$1"
