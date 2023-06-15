3d85038be81461552d6594ecc0668a47acc47261
Borislav Petrovikj <borislav.petrovikj@inplayer.com>
2023-06-15 16:24:37 +0200

    fix: added transaltion

--- src/containers/AccountModal/forms/SimultaneousLoginsNotification.tsx
+++ src/containers/AccountModal/forms/SimultaneousLoginsNotification.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useLocation, useNavigate } from 'react-router';
+import { useTranslation } from 'react-i18next';
 
 import styles from '../AccountModal.module.scss';
 
@@ -9,13 +10,14 @@ import { addQueryParam } from '#src/utils/location';
 const SimultaneousLoginsNotification = () => {
   const navigate = useNavigate();
   const location = useLocation();
+  const { t } = useTranslation('account');
   function loginClickHandler() {
     navigate(addQueryParam(location, 'u', 'login'));
   }
 
   return (
     <React.Fragment>
-      <h1 className={styles.simultaneousLoginsTitle}>You have been logged out because the simultaneous logins limit has been reached.</h1>
+      <h1 className={styles.simultaneousLoginsTitle}> {t('login.simultaneous_logins')}</h1>
       <Button type="submit" onClick={loginClickHandler} label="Ok" variant="contained" color="primary" size="large" fullWidth />
     </React.Fragment>
   );
