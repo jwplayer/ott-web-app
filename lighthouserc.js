3ba36174b114a46905942ed3ede1f085654c9729
Anton Lantukh <alantukh@jwplayer.com>
2023-07-12 10:07:58 +0200

    feat(project): lighthouse check

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,16 +1,22 @@
 module.exports = {
   ci: {
-    collect: {
-      url: ['http://127.0.0.1:4000'],
-      startServerCommand: 'http-server ./build -p 4000 -g',
-      startServerReadyPattern: 'Available on',
-      numberOfRuns: 1,
-    },
+    // collect: {
+    //   url: ['http://127.0.0.1:4000'],
+    //   startServerCommand: 'http-server ./build/public -p 4000 -g',
+    //   startServerReadyPattern: 'Available on',
+    //   numberOfRuns: 1,
+    // },
     upload: {
       target: 'temporary-public-storage',
     },
     assert: {
-      preset: 'lighthouse:no-pwa',
+      assertions: {
+        'categories:performance': ['warn', { minScore: 0.9 }],
+        'categories:seo': ['error', { minScore: 0.9 }],
+        'categories:accessibility': ['warn', { minScore: 0.9 }],
+        'categories:best-practices': ['warn', { minScore: 0.9 }],
+        'categories:pwa': ['warn', { minScore: 0.9 }],
+      },
     },
   },
 };
