fe7cad40268dc76d720bc703a1fec8c78fbbb67d
olga-jwp <obondarenko@jwplayer.com>
2023-07-21 13:54:06 +0200

    chore(project): add playlistFixture

--- src/components/CardGrid/CardGrid.test.tsx
+++ src/components/CardGrid/CardGrid.test.tsx
@@ -2,15 +2,16 @@ import * as React from 'react';
 
 import CardGrid from './CardGrid';
 
-import { generatePlaylistPlaceholder } from '#src/utils/collection';
+import playlistFixture from '#test/fixtures/playlist.json';
 import { renderWithRouter } from '#test/testUtils';
+import type { Playlist } from '#types/playlist';
 
 describe('<CardGrid>', () => {
   it('renders and matches snapshot', () => {
-    const placeholderData = generatePlaylistPlaceholder();
+    const playlist = playlistFixture as Playlist;
     const { container } = renderWithRouter(
       <CardGrid
-        playlist={placeholderData}
+        playlist={playlist}
         onCardHover={vi.fn()}
         onCardClick={vi.fn()}
         isLoading={false}
