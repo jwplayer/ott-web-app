dc22febacc36be6351306e8f4bac592c9433b44a
“Kire <k_mitrov@yahoo.com>
2023-09-15 14:12:50 +0200

    chore: post logout url

--- src/containers/OktaAuth/config.ts
+++ src/containers/OktaAuth/config.ts
@@ -4,7 +4,7 @@ const oktaConfig = {
   clientId: '0oab75w004jcsArJf5d7',
   issuer: 'https://dev-70883055.okta.com/oauth2/default',
   redirectUri: window.location.origin + '/okta/auth/callback?app-config=a2kbjdv0',
-  post_logout_url: 'http://localhost:8080/',
+  post_logout_url: 'https://ottwebapp--pr369-demo-okta-auth-xadfwiq3.web.app',
 };
 
 export const oktaAuth = new OktaAuth(oktaConfig);
