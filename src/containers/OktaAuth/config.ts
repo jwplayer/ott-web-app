0fadebf3faab0481cfe24daacebfca92be1cf8ae
“Kire <k_mitrov@yahoo.com>
2023-09-15 14:29:37 +0200

    chore: redirect uri

--- src/containers/OktaAuth/config.ts
+++ src/containers/OktaAuth/config.ts
@@ -3,7 +3,7 @@ import { OktaAuth } from '@okta/okta-auth-js';
 const oktaConfig = {
   clientId: '0oab75w004jcsArJf5d7',
   issuer: 'https://dev-70883055.okta.com/oauth2/default',
-  redirectUri: window.location.origin + '/okta/auth/callback?app-config=a2kbjdv0',
+  redirectUri: 'https://ottwebapp--pr369-demo-okta-auth-xadfwiq3.web.app/okta/auth/callback?app-config=a2kbjdv0',
   post_logout_url: 'https://ottwebapp--pr369-demo-okta-auth-xadfwiq3.web.app',
 };
 
