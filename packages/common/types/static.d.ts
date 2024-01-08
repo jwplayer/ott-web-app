62b0038d7466f47f51bba34d8eac58c61ebfaa28
Roy Schut <roy@videodock.com>
2024-01-08 14:10:26 +0100

    refactor(project): add scss typings

--- packages/common/types/static.d.ts
+++
@@ -1,8 +0,0 @@
-// @todo: should not be necessary in the common package
-declare module '*.png' {
-  const ref: string;
-  export default ref;
-}
-
-declare const __mode__: string;
-declare const __dev__: boolean;
