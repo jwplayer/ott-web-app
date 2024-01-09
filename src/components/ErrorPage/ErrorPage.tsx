faed2289d8781f7795421665e34b745f85dc3039
Anton Lantukh <alantukh@jwplayer.com>
2024-01-09 15:16:32 +0100

    fix(project): fix error state for preview and demo modes

--- src/components/ErrorPage/ErrorPage.tsx
+++ src/components/ErrorPage/ErrorPage.tsx
@@ -42,7 +42,8 @@ export const ErrorPageWithoutTranslation = ({ title, children, message, learnMor
         </header>
         <main className={styles.main}>
           <>
-            {children || <p>{message || 'Try refreshing this page or come back later.'}</p>}
+            <p className={styles.message}>{message || 'Try refreshing this page or come back later.'}</p>
+            {children}
             {(IS_DEVELOPMENT_BUILD || IS_DEMO_MODE || IS_PREVIEW_MODE) && helpLink && (
               <p className={styles.links}>
                 <a href={helpLink} target={'_blank'} rel={'noreferrer'}>
