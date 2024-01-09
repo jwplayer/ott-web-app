efffe41fc7166ac040c1018bc7a7df2840585edf
Roy Schut <roy@videodock.com>
2024-01-09 16:58:42 +0100

    refactor(project): fix configureEnv typing

--- packages/common/src/env.ts
+++ packages/common/src/env.ts
@@ -14,9 +14,9 @@ const env: Env = {
 };
 
 export const configureEnv = (options: Partial<Env>) => {
-  env.APP_VERSION ||= options.APP_VERSION;
-  env.APP_API_BASE_URL ||= options.APP_API_BASE_URL;
-  env.APP_PLAYER_ID ||= options.APP_PLAYER_ID;
+  env.APP_VERSION = options.APP_VERSION || env.APP_VERSION;
+  env.APP_API_BASE_URL = options.APP_API_BASE_URL || env.APP_API_BASE_URL;
+  env.APP_PLAYER_ID = options.APP_PLAYER_ID || env.APP_PLAYER_ID;
 
   env.APP_DEFAULT_CONFIG_SOURCE ||= options.APP_DEFAULT_CONFIG_SOURCE;
   env.APP_PLAYER_LICENSE_KEY ||= options.APP_PLAYER_LICENSE_KEY;
