fdb7e35f10759f0349a31fe6b39c04c54bea789e
Christiaan Scheermeijer <christiaan@videodock.com>
2024-01-19 14:14:39 +0100

    fix: infinite loader when buying a subscription

--- packages/common/src/stores/AccountController.ts
+++ packages/common/src/stores/AccountController.ts
@@ -453,6 +453,7 @@ export default class AccountController {
 
     useAccountStore.setState({
       subscription: activeSubscription,
+      loading: false,
       pendingOffer,
       transactions,
       activePayment,
