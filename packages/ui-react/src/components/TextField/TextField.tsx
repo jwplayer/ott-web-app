3f9df1af6194db71559fa562cccb23c564b66d1d
langemike <mikevv@gmail.com>
2024-01-25 11:24:34 +0100

    fix(a11y): prevent double ids on inputs by requiring a name
    Co-authored-by: Mike van Veenhuijzen <mike@videodock.com>

--- packages/ui-react/src/components/TextField/TextField.tsx
+++ packages/ui-react/src/components/TextField/TextField.tsx
@@ -8,14 +8,15 @@ import HelperText from '../HelperText/HelperText';
 
 import styles from './TextField.module.scss';
 
-type InputProps = Omit<React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>, 'id' | 'ref' | 'className'>;
-type TextAreaProps = Omit<React.DetailedHTMLProps<React.TextareaHTMLAttributes<HTMLTextAreaElement>, HTMLTextAreaElement>, 'id' | 'ref' | 'className'>;
+type InputProps = Omit<React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>, 'id' | 'ref' | 'className' | 'name'>;
+type TextAreaProps = Omit<React.DetailedHTMLProps<React.TextareaHTMLAttributes<HTMLTextAreaElement>, HTMLTextAreaElement>, 'id' | 'ref' | 'className' | 'name'>;
 
 type InputOrTextAreaProps =
   | ({ multiline?: never; inputRef?: RefObject<HTMLInputElement>; textAreaRef?: never } & InputProps)
   | ({ multiline: true; inputRef?: never; textAreaRef?: RefObject<HTMLTextAreaElement> } & TextAreaProps);
 
 type Props = {
+  name: string;
   className?: string;
   label?: ReactNode;
   helperText?: React.ReactNode;
@@ -28,6 +29,7 @@ type Props = {
 } & InputOrTextAreaProps;
 
 const TextField: React.FC<Props> = ({
+  name,
   className,
   label,
   error,
@@ -41,7 +43,7 @@ const TextField: React.FC<Props> = ({
   multiline,
   ...inputProps
 }: Props) => {
-  const id = useOpaqueId('text-field', inputProps.name);
+  const id = useOpaqueId('text-field', name);
   const { t } = useTranslation('common');
 
   const isInputOrTextArea = (item: unknown): item is InputOrTextAreaProps => !!item && typeof item === 'object';
@@ -60,9 +62,9 @@ const TextField: React.FC<Props> = ({
 
   const renderInput = () => {
     return isTextArea(inputProps) ? (
-      <textarea id={id} className={styles.input} rows={3} readOnly={!editing} ref={textAreaRef} {...inputProps} />
+      <textarea id={id} className={styles.input} rows={3} readOnly={!editing} ref={textAreaRef} name={name} {...inputProps} />
     ) : (
-      <input id={id} className={styles.input} type={'text'} readOnly={!editing} ref={inputRef} {...inputProps} />
+      <input id={id} className={styles.input} type={'text'} readOnly={!editing} ref={inputRef} name={name} {...inputProps} />
     );
   };
 
