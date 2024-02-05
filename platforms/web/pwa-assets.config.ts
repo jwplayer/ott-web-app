ab24b85bb501d02d8e37131f43c44ce568a38dec
langemike <mikevv@gmail.com>
2024-02-05 15:30:21 +0100

    fix(project): create missing pwa icons
    Co-authored-by: Mike van Veenhuijzen <mike@videodock.com>

--- platforms/web/pwa-assets.config.ts
+++ platforms/web/pwa-assets.config.ts
@@ -2,7 +2,7 @@ import { defineConfig, defaultAssetName, type Preset, type AssetType, type Resol
 
 export const ottPresetNoPadding: Preset = {
   transparent: {
-    sizes: [196, 160, 144, 96, 64, 32],
+    sizes: [196, 192, 160, 144, 96, 64, 32, 16],
     favicons: [[48, 'favicon.ico']],
     padding: 0,
   },
