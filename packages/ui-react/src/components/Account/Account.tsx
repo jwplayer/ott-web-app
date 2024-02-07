e50807b3b4a770f566c04961b340c0de8a96e752
Melissa 't Hart <melissa@videodock.com>
2024-02-07 13:48:07 +0100

    refactor(a11y): remove double account header and add translations

--- packages/ui-react/src/components/Account/Account.tsx
+++ packages/ui-react/src/components/Account/Account.tsx
@@ -233,7 +233,6 @@ const Account = ({ panelClassName, panelHeaderClassName, canUpdateEmail = true }
             },
             content: (section) => (
               <>
-                <h1 className={styles.hideUntilFocus}>{t('nav.account')}</h1>
                 <TextField
                   name="firstName"
                   label={t('account.firstname')}
