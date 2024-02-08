ef4d3fe407cb2c0d72edd23a1fd7a1276e83d460
Melissa Hart <48496458+MelissaDTH@users.noreply.github.com>
2024-02-08 08:31:56 +0100

    feat: underline for active header item and add lineair gradient

--- packages/ui-react/stylelint.config.js
+++ packages/ui-react/stylelint.config.js
@@ -1,3 +1,11 @@
 module.exports = {
   extends: ['stylelint-config-jwp'],
+  rules: {
+    'selector-pseudo-class-no-unknown': [
+      true,
+      {
+        ignorePseudoClasses: ['global'],
+      },
+    ],
+  },
 };
