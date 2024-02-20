86be1941ced555ba177b1a021013d0099693fa34
Melissa Hart <48496458+MelissaDTH@users.noreply.github.com>
2024-02-20 16:52:28 +0100

    fix(a11y): roles and aria-attributes regarding modals (#125)

--- packages/ui-react/src/containers/TrailerModal/TrailerModal.tsx
+++ packages/ui-react/src/containers/TrailerModal/TrailerModal.tsx
@@ -28,7 +28,7 @@ const TrailerModal: React.FC<Props> = ({ item, open, title, onClose }) => {
 
   return (
     <Modal open={open} onClose={onClose}>
-      <div className={styles.container} role="dialog" aria-labelledby="trailer-modal-title">
+      <div className={styles.container} role="dialog" aria-modal="true" aria-labelledby="trailer-modal-title">
         <Player
           item={item}
           onPlay={handlePlay}
