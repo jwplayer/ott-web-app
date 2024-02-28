f57e1bf13a7b14c347e19705489566b70aadede5
Melissa Hart <48496458+MelissaDTH@users.noreply.github.com>
2024-02-28 15:51:05 +0100

    fix(project): overall styling of the hub page

--- packages/ui-react/src/components/Hero/Hero.tsx
+++ packages/ui-react/src/components/Hero/Hero.tsx
@@ -1,5 +1,8 @@
+import classNames from 'classnames';
 import React from 'react';
 
+import useBreakpoint, { Breakpoint } from '../../hooks/useBreakpoint';
+import CollapsibleText from '../CollapsibleText/CollapsibleText';
 import Image from '../Image/Image';
 
 import styles from './Hero.module.scss';
@@ -11,12 +14,17 @@ type Props = {
 };
 
 const Hero = ({ title, description, image }: Props) => {
+  const breakpoint: Breakpoint = useBreakpoint();
+  const isMobile = breakpoint === Breakpoint.xs;
+  const alt = ''; // intentionally empty for a11y, because adjacent text alternative
+
   return (
-    <div className={styles.hero}>
-      <Image className={styles.image} image={image} width={1280} alt="" />
-      <div className={styles.content}>
+    <div className={classNames(styles.hero, styles.heroPadding)}>
+      <Image className={styles.poster} image={image} width={1280} alt={alt} />
+      <div className={styles.posterFade} />
+      <div className={styles.info}>
         <h1 className={styles.title}>{title}</h1>
-        <p>{description}</p>
+        <CollapsibleText text={description} className={styles.description} maxHeight={isMobile ? 60 : 'none'} />
       </div>
     </div>
   );
