d510fd7b08698a5a90b553b26bd4b002c4f7e13d
Carina Dragan <carinadragan10@gmail.com>
2025-04-11 14:35:11 +0300

    feat: add filter code, filter logic to be implemented

--- packages/ui-react/src/hooks/useFilteredContent.ts
+++ packages/ui-react/src/hooks/useFilteredContent.ts
@@ -1,11 +1,64 @@
 import type { Content } from '@jwp/ott-common/types/config';
 import useBreakpoint, { Breakpoint } from '@jwp/ott-ui-react/src/hooks/useBreakpoint';
 
-// TODO: Add more filtering logic here
-export const useFilteredContent = (content: Content[]) => {
+const DEVICE_FILTER_LABELS = {
+  mobile: 'mobile',
+  tablet: 'tablet',
+  desktop: 'desktop',
+};
+
+const COUNTRY_FILTER_LABELS = {
+  nl: 'NL',
+  usa: 'USA',
+  ca: 'CA',
+};
+
+const GENRE_FILTER_LABELS = {
+  comedy: 'comedy',
+  drama: 'drama',
+  action: 'action',
+};
+
+const useDeviceType = () => {
   const breakpoint = useBreakpoint();
   const isMobile = breakpoint < Breakpoint.md;
-  const filteredContent = isMobile ? content : content?.filter((item) => !item.filterTags?.includes('mobile'));
+  const isTablet = breakpoint >= Breakpoint.md && breakpoint < Breakpoint.lg;
+  const isDesktop = breakpoint >= Breakpoint.lg;
 
-  return filteredContent;
+  return { isMobile, isTablet, isDesktop };
 };
+
+const useFilterContentByDevice = (content: Content[]) => {
+  const { isMobile, isTablet, isDesktop } = useDeviceType();
+
+  return content?.filter((item) => {
+    if (isMobile && item.filterTags?.includes(DEVICE_FILTER_LABELS.mobile)) return true;
+    if (isTablet && item.filterTags?.includes(DEVICE_FILTER_LABELS.tablet)) return true;
+    if (isDesktop && item.filterTags?.includes(DEVICE_FILTER_LABELS.desktop)) return true;
+    return true;
+  });
+};
+
+const filterContentByCountry = (content: Content[]) => {
+  return content?.filter((item) => {
+    if (item.filterTags?.includes(COUNTRY_FILTER_LABELS.nl)) return true;
+    if (item.filterTags?.includes(COUNTRY_FILTER_LABELS.usa)) return true;
+    if (item.filterTags?.includes(COUNTRY_FILTER_LABELS.ca)) return true;
+    return true;
+  });
+};
+
+const filterContentByGenre = (content: Content[]) => {
+  return content?.filter((item) => {
+    if (item.filterTags?.includes(GENRE_FILTER_LABELS.action)) return true;
+    if (item.filterTags?.includes(GENRE_FILTER_LABELS.comedy)) return true;
+    if (item.filterTags?.includes(GENRE_FILTER_LABELS.drama)) return true;
+    return true;
+  });
+};
+
+export const useFilteredContent = (content: Content[]) => [
+  ...useFilterContentByDevice(content),
+  ...filterContentByCountry(content),
+  ...filterContentByGenre(content),
+];
