bf354536ad0ab977216c0b0fc98d2d68a1d219a9
Carina Dragan <carinadragan10@gmail.com>
2025-04-14 12:03:03 +0300

    feat: add filtering based on day of week

--- packages/ui-react/src/hooks/useFilteredContent.ts
+++ packages/ui-react/src/hooks/useFilteredContent.ts
@@ -7,6 +7,16 @@ const DEVICE_FILTER_LABELS = {
   desktop: 'desktop-device',
 };
 
+const WEEK_DAY_FILTER_LABELS = {
+  Monday: 'Monday',
+  Tuesday: 'Tuesday',
+  Wednesday: 'Wednesday',
+  Thursday: 'Thursday',
+  Friday: 'Friday',
+  Saturday: 'Saturday',
+  Sunday: 'Sunday',
+};
+
 const useDeviceType = () => {
   const breakpoint = useBreakpoint();
   const isMobile = breakpoint < Breakpoint.md;
@@ -16,9 +26,7 @@ const useDeviceType = () => {
   return { isMobile, isTablet, isDesktop };
 };
 
-export const useFilterContent = (content: Content[]) => {
-  const { isMobile, isTablet, isDesktop } = useDeviceType();
-
+const filterContentByDevice = (content: Content[], isMobile: boolean, isTablet: boolean, isDesktop: boolean) => {
   const deviceFilterMap = {
     [DEVICE_FILTER_LABELS.mobile]: isMobile,
     [DEVICE_FILTER_LABELS.tablet]: isTablet,
@@ -26,4 +34,29 @@ export const useFilterContent = (content: Content[]) => {
   };
 
   return content?.filter((item) => Object.entries(deviceFilterMap).every(([label, isActive]) => isActive || !item.filterTags?.includes(label)));
+};
+
+const filterContentByWeekDay = (content: Content[]) => {
+  const currentDay = new Date().toLocaleString('en-US', { weekday: 'long' });
+
+  const weekDayFilterMap = {
+    [WEEK_DAY_FILTER_LABELS.Monday]: currentDay === WEEK_DAY_FILTER_LABELS.Monday,
+    [WEEK_DAY_FILTER_LABELS.Tuesday]: currentDay === WEEK_DAY_FILTER_LABELS.Tuesday,
+    [WEEK_DAY_FILTER_LABELS.Wednesday]: currentDay === WEEK_DAY_FILTER_LABELS.Wednesday,
+    [WEEK_DAY_FILTER_LABELS.Thursday]: currentDay === WEEK_DAY_FILTER_LABELS.Thursday,
+    [WEEK_DAY_FILTER_LABELS.Friday]: currentDay === WEEK_DAY_FILTER_LABELS.Friday,
+    [WEEK_DAY_FILTER_LABELS.Saturday]: currentDay === WEEK_DAY_FILTER_LABELS.Saturday,
+    [WEEK_DAY_FILTER_LABELS.Sunday]: currentDay === WEEK_DAY_FILTER_LABELS.Sunday,
+  };
+
+  return content?.filter((item) => Object.entries(weekDayFilterMap).every(([label, isActive]) => isActive || !item.filterTags?.includes(label)));
+};
+
+export const useFilterContent = (content: Content[]) => {
+  const { isMobile, isTablet, isDesktop } = useDeviceType();
+
+  const filteredContentByDevice = filterContentByDevice(content, isMobile, isTablet, isDesktop);
+  const filteredContentByWeekDay = filterContentByWeekDay(content);
+
+  return filteredContentByDevice?.filter((item) => filteredContentByWeekDay?.includes(item));
 };
