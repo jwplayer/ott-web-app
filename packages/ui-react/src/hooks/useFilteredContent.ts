533817e196eda6bb2fed8b085bf6b57486070839
Carina Dragan <carinadragan10@gmail.com>
2025-04-16 11:47:22 +0300

    feat: simplify filtering, fix logic to check multiple labels

--- packages/ui-react/src/hooks/useFilteredContent.ts
+++ packages/ui-react/src/hooks/useFilteredContent.ts
@@ -7,16 +7,6 @@ const DEVICE_FILTER_LABELS = {
   desktop: 'desktop-device',
 };
 
-const WEEK_DAY_FILTER_LABELS = {
-  Monday: 'Monday',
-  Tuesday: 'Tuesday',
-  Wednesday: 'Wednesday',
-  Thursday: 'Thursday',
-  Friday: 'Friday',
-  Saturday: 'Saturday',
-  Sunday: 'Sunday',
-};
-
 const COUNTRIES = {
   DE: 'Germany',
   FR: 'France',
@@ -67,53 +57,20 @@ const getCountryByTimezone = () => {
   return countryTimezone ? COUNTRIES[countryTimezone] : undefined;
 };
 
-const filterContentByDevice = (content: Content[], isMobile: boolean, isTablet: boolean, isDesktop: boolean) => {
-  const deviceFilterMap = {
-    [DEVICE_FILTER_LABELS.mobile]: isMobile,
-    [DEVICE_FILTER_LABELS.tablet]: isTablet,
-    [DEVICE_FILTER_LABELS.desktop]: isDesktop,
-  };
-
-  return content?.filter((item) => Object.entries(deviceFilterMap).every(([label, isActive]) => isActive || !item.filterTags?.includes(label)));
-};
-
-const filterContentByWeekDay = (content: Content[]) => {
-  const currentDay = new Date().toLocaleString('en-US', { weekday: 'long' });
-
-  const weekDayFilterMap = {
-    [WEEK_DAY_FILTER_LABELS.Monday]: currentDay === WEEK_DAY_FILTER_LABELS.Monday,
-    [WEEK_DAY_FILTER_LABELS.Tuesday]: currentDay === WEEK_DAY_FILTER_LABELS.Tuesday,
-    [WEEK_DAY_FILTER_LABELS.Wednesday]: currentDay === WEEK_DAY_FILTER_LABELS.Wednesday,
-    [WEEK_DAY_FILTER_LABELS.Thursday]: currentDay === WEEK_DAY_FILTER_LABELS.Thursday,
-    [WEEK_DAY_FILTER_LABELS.Friday]: currentDay === WEEK_DAY_FILTER_LABELS.Friday,
-    [WEEK_DAY_FILTER_LABELS.Saturday]: currentDay === WEEK_DAY_FILTER_LABELS.Saturday,
-    [WEEK_DAY_FILTER_LABELS.Sunday]: currentDay === WEEK_DAY_FILTER_LABELS.Sunday,
-  };
-
-  return content?.filter((item) => Object.entries(weekDayFilterMap).every(([label, isActive]) => isActive || !item.filterTags?.includes(label)));
-};
-
-const filterContentByCountry = (content: Content[]) => {
-  const country = getCountryByTimezone();
-
-  if (!country) return content;
-
-  const countryFilterMap = {
-    [COUNTRIES.DE]: country === COUNTRIES.DE,
-    [COUNTRIES.FR]: country === COUNTRIES.FR,
-    [COUNTRIES.NL]: country === COUNTRIES.NL,
-    [COUNTRIES.RO]: country === COUNTRIES.RO,
-  };
-
-  return content?.filter((item) => Object.entries(countryFilterMap).every(([label, isActive]) => isActive || !item.filterTags?.includes(label)));
-};
+const getCurrentDay = () => new Date().toLocaleString('en-US', { weekday: 'long' });
 
 export const useFilterContent = (content: Content[]) => {
   const { isMobile, isTablet, isDesktop } = useDeviceType();
+  const currentDay = getCurrentDay();
+  const country = getCountryByTimezone();
 
-  const filteredContentByDevice = filterContentByDevice(content, isMobile, isTablet, isDesktop);
-  const filteredContentByWeekDay = filterContentByWeekDay(content);
-  const filteredContentByCountry = filterContentByCountry(content);
-
-  return filteredContentByDevice.filter((item) => filteredContentByWeekDay.includes(item) && filteredContentByCountry.includes(item));
+  return content?.filter((item) => {
+    if (item?.filterTags?.length === 0) return true;
+    if (item?.filterTags?.includes(DEVICE_FILTER_LABELS.mobile) && isMobile) return true;
+    if (item?.filterTags?.includes(DEVICE_FILTER_LABELS.tablet) && isTablet) return true;
+    if (item?.filterTags?.includes(DEVICE_FILTER_LABELS.desktop) && isDesktop) return true;
+    if (item?.filterTags?.includes(currentDay)) return true;
+    if (country && item?.filterTags?.includes(country)) return true;
+    return false;
+  });
 };
