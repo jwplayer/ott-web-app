21d9ef08aa5c94a87079183af28eb84a3a7cedbf
Carina Dragan <carinadragan10@gmail.com>
2025-04-29 15:44:33 +0200

    feat: use isTruthyCustomParamValue function

--- packages/ui-react/src/pages/Home/Home.tsx
+++ packages/ui-react/src/pages/Home/Home.tsx
@@ -4,6 +4,7 @@ import type { Content } from '@jwp/ott-common/types/config';
 import { useConfigStore } from '@jwp/ott-common/src/stores/ConfigStore';
 import { useTranslation } from 'react-i18next';
 import { useFilterContent } from '@jwp/ott-ui-react/src/hooks/useFilteredContent';
+import { isTruthyCustomParamValue } from '@jwp/ott-common/src/utils/common';
 
 import ShelfList from '../../containers/ShelfList/ShelfList';
 
@@ -11,7 +12,7 @@ const Home = () => {
   const { config } = useConfigStore(({ config, accessModel }) => ({ config, accessModel }), shallow);
   const content: Content[] = config?.content;
   const { t } = useTranslation('common');
-  const labelsFilteringEnabled = Boolean(config?.custom?.enableLabelsFiltering);
+  const labelsFilteringEnabled = isTruthyCustomParamValue(config?.custom?.enableLabelsFiltering);
   const configContent = useFilterContent({ content, labelsFilteringEnabled });
 
   return (
