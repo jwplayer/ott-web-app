2b971a674b3434024e43bb2c1ac4d48504e6d29e
Carina Dragan <carinadragan10@gmail.com>
2025-06-23 17:04:20 +0200

    feat: add more tests for video clicking

--- platforms/web/test-e2e/tests/content_list_test.ts
+++ platforms/web/test-e2e/tests/content_list_test.ts
@@ -18,3 +18,20 @@ Scenario('Header button navigates to content list screen', async ({ I }) => {
   I.see('All Popular');
   I.seeInCurrentUrl(`${constants.baseUrl}n/`);
 });
+
+Scenario('Content list screen loads', async ({ I }) => {
+  I.seeElement('div[role="grid"]');
+  I.seeElement('div[role="gridcell"]');
+  I.seeElement('div[id="layout_grid_0-0"]');
+});
+
+Scenario('I can click on first card and navigate to the video details screen', async ({ I }) => {
+  I.click('div[id="layout_grid_0-0"]');
+  I.see(constants.startWatchingButton);
+  I.click(constants.startWatchingButton);
+  I.seeElement('video');
+
+  I.click('video');
+  I.click('button[aria-label="Back"]');
+  await I.checkPlayerClosed();
+});
