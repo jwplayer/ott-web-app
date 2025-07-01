34154148c15fc7da7aa90276fbad0df017c5e026
Christiaan Scheermeijer <christiaan@videodock.com>
2025-07-01 16:45:43 +0200

    chore(deps): upgrade inversify to 7.5.4

--- packages/common/src/modules/functions/getApiAccessBridgeUrl.ts
+++ packages/common/src/modules/functions/getApiAccessBridgeUrl.ts
@@ -1,4 +1,4 @@
-import type { interfaces } from 'inversify';
+import type { ResolutionContext } from 'inversify';
 
 import AppController from '../../controllers/AppController';
 
@@ -7,6 +7,6 @@ import AppController from '../../controllers/AppController';
  * If the access bridge URL is defined in the application's .ini configuration file,
  * the function returns the URL. If the value is not defined, it returns `undefined`.
  */
-export const getApiAccessBridgeUrl = (context: interfaces.Context) => {
-  return context.container.get(AppController).getApiAccessBridgeUrl();
+export const getApiAccessBridgeUrl = (context: ResolutionContext) => {
+  return context.get(AppController).getApiAccessBridgeUrl();
 };
--- packages/ui-react/src/modules/container.ts
+++ packages/ui-react/src/modules/container.ts
@@ -1,5 +1,5 @@
 import { Container } from 'inversify';
 
-const container = new Container({ defaultScope: 'Singleton', skipBaseClassChecks: true });
+const container = new Container({ defaultScope: 'Singleton', autobind: true });
 
 export { container };
